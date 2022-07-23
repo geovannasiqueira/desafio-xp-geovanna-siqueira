0235b98212a085bf86811d34cb1211495478c8fc
Geovanna Carolline <geovannacarolline@pop-os.localdomain>
2022-07-23 12:36:37 -0400

    refactor(loading): improve loading message

--- src/components/StocksTable.js
+++ src/components/StocksTable.js
@@ -22,16 +22,24 @@ function StocksTable() {
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-xl font-bold mb-6">Disponíveis para investir</h2>
-      <table className="table table-zebra inline-flex flex-col gap-2 w-96 p-12 bg-white rounded-md shadow-xl ">
-        <thead>
-          <tr>
-            <th>Ação</th>
-            <th>Qtde</th>
-            <th>Valor( R$ )/un</th>
-            <th>Negociar</th>
-          </tr>
-        </thead>
-        {stocks.length === 0 ? <p>Loading...</p> : (
+      {stocks.length === 0 ? (
+        <div className="px-4 my-6">
+          <div class="alert shadow-lg flex flex-col justify-around">
+            <h3 class="font-bold">Loading...</h3>
+            <progress class="progress w-28"></progress>
+          </div>
+        </div>
+      ) : (
+        <table className="table table-zebra inline-flex flex-col gap-2 w-96 p-12 bg-white rounded-md shadow-xl ">
+          <thead>
+            <tr>
+              <th>Ação</th>
+              <th>Qtde</th>
+              <th>Valor( R$ )/un</th>
+              <th>Negociar</th>
+            </tr>
+          </thead>
+
           <tbody>
             {stocks &&
               stocks.map((stock, key) => (
@@ -52,8 +60,8 @@ function StocksTable() {
                 </tr>
               ))}
           </tbody>
-        )}
-      </table>
+        </table>
+      )}
     </div>
   );
 }
